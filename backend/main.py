6b5bef8805d371b38a3c63f13f3376568b49a8a8
Adam <adam@adammillington.com>
2026-03-14 18:40:58 +0000

    Updated backend and created template file

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="brainLLM", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
